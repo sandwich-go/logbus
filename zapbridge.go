a4f2d89faabc62bb1f39d8a48feb4aa622e7e220
biyongze <yongze.bi@centurygame.com>
2022-11-17 15:03:03 +0800

    feat: add zapbridge

---
+++ zapbridge.go
@@ -0,0 +1,82 @@
+package logbus
+
+import (
+	"fmt"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"time"
+)
+
+type Field = zap.Field
+
+func String(key string, val string) Field {
+	return zap.String(key, val)
+}
+
+func Binary(key string, val []byte) Field {
+	return zap.Binary(key, val)
+}
+
+func Bool(key string, val bool) Field {
+	return zap.Bool(key, val)
+}
+
+func Float64(key string, val float64) Field {
+	return zap.Float64(key, val)
+}
+
+func Float32(key string, val float32) Field {
+	return zap.Float32(key, val)
+}
+
+func Int(key string, val int) Field {
+	return zap.Int(key, val)
+}
+
+func Int64(key string, val int64) Field {
+	return zap.Int64(key, val)
+}
+
+func Int32(key string, val int32) Field {
+	return zap.Int32(key, val)
+}
+
+func Uint(key string, val uint) Field {
+	return zap.Uint(key, val)
+}
+
+func Uint64(key string, val uint64) Field {
+	return zap.Uint64(key, val)
+}
+
+func Uint32(key string, val uint32) Field {
+	return zap.Uint32(key, val)
+}
+
+func Uint16(key string, val uint16) Field {
+	return zap.Uint16(key, val)
+}
+
+func Reflect(key string, val interface{}) Field {
+	return zap.Reflect(key, val)
+}
+
+func Stringer(key string, val fmt.Stringer) Field {
+	return zap.Stringer(key, val)
+}
+
+func Time(key string, val time.Time) Field {
+	return zap.Time(key, val)
+}
+
+func Stack(key string) Field {
+	return zap.Stack(key)
+}
+
+func Duration(key string, val time.Duration) Field {
+	return zap.Duration(key, val)
+}
+
+func Object(key string, val zapcore.ObjectMarshaler) Field {
+	return zap.Object(key, val)
+}
