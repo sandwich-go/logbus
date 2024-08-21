7240e6988f88e5b00256e73b2559c47d443a6ab0
biyongze <yongze.bi@centurygame.com>
2024-08-21 17:38:08 +0800

    fix: tga时区偏移量错误

--- thinkingdata/init.go
+++ thinkingdata/init.go
@@ -16,7 +16,7 @@ func init() {
 	if os.Getenv(envTGATimeZoneOffset) != "" {
 		offset, err := strconv.ParseInt(os.Getenv(envTGATimeZoneOffset), 10, 64)
 		xpanic.WhenTrue(err != nil, fmt.Sprintf("TGA_TIMEZONE_OFFSET %s is not number", os.Getenv(envTGATimeZoneOffset)))
-		xpanic.WhenTrue(offset > 12*60 || offset < -12*60, fmt.Sprintf("TGA_TIMEZONE_OFFSET %d is out of range", offset))
+		xpanic.WhenTrue(offset > 12*60*60 || offset < -12*60*60, fmt.Sprintf("TGA_TIMEZONE_OFFSET %d is out of range", offset))
 		locationTGA = time.FixedZone("", int(offset))
 	}
 }
